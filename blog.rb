5d7cdb1866f06a36e1c9cb751e8d6e078aabccb2
Calum Gilchrist <calum@homelinen.org>
2015-02-15 01:03:26 +0000

    Added Post Permalinks

--- blog.rb
+++ blog.rb
@@ -26,15 +26,36 @@ get '/css/main.css' do
    sass :main, :style => :expanded 
 end
 
+get '/post/:date/:name' do
+    date = params[:date]
+    name = params[:name]
+
+    post_name = "content/#{date}-#{name}.md"
+    @articles = Dir['content/*.md']
+    if @articles.include? post_name
+        post = parse_article(post_name)
+        @date = post[:date]
+        @title = post[:title]
+        haml :layout, layout: false do 
+            markdown post[:content].join("")
+        end
+    else
+        status 404
+        haml "%h1 404"
+    end
+
+end
+
 def parse_article(filename)
-    match = /\d{4}-\d{2}-\d{2}/.match(filename)
+    match = /(\d{4}-\d{2}-\d{2})-([\w\d\-_]+)/.match(filename)
     
     if match
 
         file = File.new(filename)
         yaml = YAML.load(file)
 
-        date = Date.parse(match[0])
+        date = Date.parse(match[1])
+        title = match[2]
         c = 0
         file.rewind
         minus_front = file.readlines.select do |l| 
@@ -47,7 +68,8 @@ def parse_article(filename)
         end
         {
             content: minus_front,
-            date: date
+            date: date,
+            title: title
         }
     end
 end
--- content/2015-02-13-first-post.md
+++ content/2015-02-13-first-post.md
@@ -1,7 +1,3 @@
 ---
-title: First
 ---
-
-## First Post
-
 My first post
